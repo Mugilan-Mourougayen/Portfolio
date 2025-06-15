e058a1e9be8a81a469380f7dc4c2eb15aded463f
Mugilan Mourougayen <mugilanmourougayen@gmail.com>
2025-06-15 04:48:39 +0200

    testing

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,28 @@
-import React from 'react'
-import Profilepi from './Profilepi';
+import React from "react";
+import Profilepi from "./Profilepi";
 
 const Home = () => {
+  let num = 5;
+  const nums = num + 1;
   return (
-    <div className='mainbox'>
-      <Profilepi/>
-      <br/>
+    <div className="mainbox">
+      <Profilepi />
+      <br />
       <p>
-        Hi,<span class="wave">👋</span> I am  <span className='name'>MUGILAN</span>,
+        Hi,<span class="wave">👋</span> I am{" "}
+        <span className="name">MUGILAN</span>,
       </p>
-      <h1 > I am a 
-         <span className='job'> Full STACK DEVELOPER </span>
+      <h1>
+        {" "}
+        I am a<span className="job"> Full STACK DEVELOPER </span>
       </h1>
-      <div className='about'>
-
-      I'm a web enthusiast, diving deep into Full Stack development, and always on the hunt for exciting web adventures. Let's code some digital magic together!
+      <div className="about">
+        I'm a web enthusiast, diving deep into Full Stack development, and
+        always on the hunt for exciting web adventures. Let's code some digital
+        magic together!
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
